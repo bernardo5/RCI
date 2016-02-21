ce5c3f14f58cb559479c6e3b4c3d64a5dbb8d324
bernardo5 <bdnabaisgomes@yahoo.com>
2016-02-21 13:10:04 +0000

    criacao ficheiro p 3 tarefa

---
+++ labs/proj_guide/test2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <errno.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(){
+	struct hostent *h;
+	struct in_addr *a;
+	
+	if((h=gethostbyname("tejo.tecnico.ulisboa.pt"))==NULL)exit(1);//error
+	
+	printf("official host name:%s\n", h->h_name);
+		
+	a=(struct in_addr*)h->h_addr_list[0];
+	
+	printf("internet address: %s (%081X)\n", inet_ntoa(*a), (long unsigned int)ntohl(a->s_addr));
+	
+	exit(0);
+}

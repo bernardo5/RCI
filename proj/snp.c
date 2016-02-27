c8374a852e889763d298f8887c68a576a9cd9810
bernardo5 <bdnabaisgomes@yahoo.com>
2016-02-27 17:25:30 +0000

    a funcionar sem bugs

--- proj/snp.c
+++ proj/snp.c
@@ -47,7 +47,7 @@ int find_user(user*root, char*name, char**buf){
 				  }
 		}
 	}else{
-		 strcpy(*buf, "NOK - Dont hack me bitch\0");
+		 strcpy(*buf, "NOK - Dont hack me. Thank you\0");
 		 return 0; /* name does not exist */
 	}
 }

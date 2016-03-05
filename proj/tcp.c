add620cae396b8b6cafaa159690e0b55c36be6c8
bernardo5 <bdnabaisgomes@yahoo.com>
2016-03-05 16:12:55 +0000

    tcp com echo ok

---
+++ proj/tcp.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+int main(){
+	int fd, nbytes, nleft, nwritten, nread, n;
+	char*ptr, buffer[128];
+	struct sockaddr_in addr;
+	int addrlen;
+	
+	fd=socket(AF_INET, SOCK_STREAM,0);
+	if(fd==-1){printf("error opening socket\n");exit(-1);}
+
+	memset((void*)&addr, (int)'\0', sizeof(addr));
+
+	addr.sin_family=AF_INET;
+	inet_aton("192.168.1.71", &addr.sin_addr);
+	addr.sin_port=htons(9000);
+	
+	n=connect(fd,(struct sockaddr*)&addr, sizeof(addr));
+	if(n==-1){printf("erro no connect\n"); exit(1);}
+
+	ptr=strcpy(buffer,"Hello!\n");
+	nbytes=7;
+
+	nleft=nbytes;
+	while(nleft>0){nwritten=write(fd,ptr,nleft);
+		if(nwritten<=0)exit(1);//error
+		nleft-=nwritten;
+		ptr+=nwritten;}
+	nleft=nbytes; ptr=buffer;
+	while(nleft>0){nread=read(fd,ptr,nleft);
+		if(nread==-1)exit(1);//error
+		else if(nread==0)break;//closed by peer
+		nleft-=nread;
+		ptr+=nread;}
+	nread=nbytes-nleft;
+	close(fd);
+	write(1,"echo: ",6);//stdout
+	write(1,buffer,nread);
+	
+
+	exit(0);
+}
